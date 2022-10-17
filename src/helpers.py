57ae2985cd97bacc2690fcb2b170ed25a17bead7
Monnet Joris <joris.monnet@epfl.ch>
2022-10-17 11:22:26 +0200

    format src

--- src/helpers.py
+++ src/helpers.py
@@ -4,25 +4,34 @@ import numpy as np
 
 
 def load_data(path_dataset, sub_sample=False):
-    """
-
-    """
+    """ """
 
     # With 2 IO access
-    data = np.genfromtxt(path_dataset, delimiter=',', encoding='utf-8',
-            skip_header=1, max_rows=50 if sub_sample else None)
-    x =  data[:, 2:].copy()
+    data = np.genfromtxt(
+        path_dataset,
+        delimiter=",",
+        encoding="utf-8",
+        skip_header=1,
+        max_rows=50 if sub_sample else None,
+    )
+    x = data[:, 2:].copy()
 
     # Load only the first column to generate the first array
-    y_raw = np.genfromtxt(path_dataset, delimiter=',', encoding='utf-8',
-            skip_header=1, max_rows=50 if sub_sample else None, 
-            usecols=[1], dtype=np.string_)
+    y_raw = np.genfromtxt(
+        path_dataset,
+        delimiter=",",
+        encoding="utf-8",
+        skip_header=1,
+        max_rows=50 if sub_sample else None,
+        usecols=[1],
+        dtype=np.string_,
+    )
 
     y = []
     for sample in y_raw:
-        y.append(0 if sample==bytes('b','ascii') else 1)
+        y.append(0 if sample == bytes("b", "ascii") else 1)
     y = np.array(y)[:, np.newaxis]
-    
+
     return x, y
 
     # With 1 IO access
@@ -37,7 +46,7 @@ def load_data(path_dataset, sub_sample=False):
     # for row_idx, sample in enumerate(data.flat):
     #     x[row_idx] = np.array(sample.tolist()[2:], dtype=float)
     #     y[row_idx] = 0 if sample[1]=='b' else 1
-    
+
     # return x, y
 
 
@@ -49,9 +58,11 @@ def standardize_training(x, missing_values=True):
     x = x / std_x
     return x, mean_x, std_x
 
+
 def standardize_test(x, mean_x, std_x):
     """"""
     return (x - mean_x) / std_x
+
 
 def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
     """
@@ -80,21 +91,19 @@ def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
 
 
 def add_offset(x):
-    """
-
-    """
+    """ """
     return np.hstack((np.ones(x.shape[0])[:, np.newaxis], x))
 
 
 def replace_nan_by_means(data, nan_value=-999.0, mean_data=None):
     """
 
-        mean_dataset : use it if the value has already been computed
-            array of shape (D) (contains the mean of each feature column)
+    mean_dataset : use it if the value has already been computed
+        array of shape (D) (contains the mean of each feature column)
 
-        Test :  arr_test = np.array([[1, 2, 3, 4], [10, np.nan, 11, 12], [np.nan, 13, 14, np.nan], [np.nan, 15, 16, 17]])
-                arr_test_theoric = replace_nan_by_means(arr_test)
-                assert(np.allclose(arr_test_theoric[1, 1], np.nanmean(arr_test[:, 1]))) #, "mean not computed correctly"
+    Test :  arr_test = np.array([[1, 2, 3, 4], [10, np.nan, 11, 12], [np.nan, 13, 14, np.nan], [np.nan, 15, 16, 17]])
+            arr_test_theoric = replace_nan_by_means(arr_test)
+            assert(np.allclose(arr_test_theoric[1, 1], np.nanmean(arr_test[:, 1]))) #, "mean not computed correctly"
     """
 
     for col_idx in range(data.shape[1]):
@@ -103,20 +112,23 @@ def replace_nan_by_means(data, nan_value=-999.0, mean_data=None):
 
     return data
 
-#Replace each degree feature by a feature of it's sine and one of it's cosine
-def expand_degree(x,ID_feature):
-    x=np.c_[x,np.cos(x[:,ID_feature])]
-    x[:,ID_feature] = np.sin(x[:,ID_feature])
+
+# Replace each degree feature by a feature of it's sine and one of it's cosine
+def expand_degree(x, ID_feature):
+    x = np.c_[x, np.cos(x[:, ID_feature])]
+    x[:, ID_feature] = np.sin(x[:, ID_feature])
     return x
 
-#For all degree features
-def expand_degrees(x,IDs_Features):
-    for ids in IDs_Features :
-        x=expand_degree(x,ids)
+
+# For all degree features
+def expand_degrees(x, IDs_Features):
+    for ids in IDs_Features:
+        x = expand_degree(x, ids)
     return x
+
 
 def build_poly(x, degree):
     poly = np.ones((len(x), 1))
-    for deg in range(1, degree+1):
+    for deg in range(1, degree + 1):
         poly = np.c_[poly, np.power(x, deg)]
     return poly
