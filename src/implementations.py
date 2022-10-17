11c16138b64fcd0d89e376d4ad84402955d5436f
Monnet Joris <joris.monnet@epfl.ch>
2022-10-17 11:10:55 +0200

    update resources

--- src/implementations.py
+++ src/implementations.py
@@ -6,6 +6,8 @@
 import numpy as np
 import math
 from helpers import batch_iter,build_poly
+
+
 #
 #   MANDATORY FUNCTIONS
 # 
@@ -33,7 +35,7 @@ def least_squares(y, tx) :
 def ridge_regression(y, tx, lambda_) :
     """Ridge regression using normal equations"""
     N, D = tx.shape
-    w = np.linalg.solve(tx.T @ tx + lambda_*2*N*np.eye(D), tx.T@y)
+    w = np.linalg.solve(tx.T @ tx + lambda_ * 2 * N * np.eye(D), tx.T@y)
     return w, compute_mse(y, tx, w)
 
 def logistic_regression(y, tx, initial_w, max_iters, gamma):
@@ -52,7 +54,7 @@ def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
         if len(lossList) > 1 and np.abs(lossList[-1] - lossList[-2]) < treshold:
             break #converge criterion
 
-    return w, lossList[-1]
+    return w, lossList[-1] #take only the last loss
 
 def cross_validation(y, x, k_indices, k, lambda_, degree):
     """return the loss of ridge regression for a fold corresponding to k_indices
@@ -82,8 +84,8 @@ def cross_validation(y, x, k_indices, k, lambda_, degree):
 
     w = ridge_regression(y_tr,tx_tr,lambda_)
 
-    loss_tr = math.sqrt(2*compute_mse(y_tr,tx_tr,w))
-    loss_te = math.sqrt(2*compute_mse(y_te,tx_te,w))
+    loss_tr = math.sqrt(2 * compute_mse(y_tr,tx_tr,w))
+    loss_te = math.sqrt(2 * compute_mse(y_te,tx_te,w))
     return loss_tr, loss_te
 
 #
@@ -94,9 +96,10 @@ def compute_gradient(y,tx,w):
     return - tx.T.dot(y - tx @ w) / len(y)
 
 def compute_mse(y,tx,w):
-    return 0.5*np.mean((y - tx @ w)**2)
+    return 0.5 * np.mean((y - tx @ w) ** 2)
 
 def compute_log_loss(y,tx,w,lambda_):
+    "Compute loss with log"
     return np.sum(np.log(1 + np.exp(tx @ w))) - (y.T @ (tx @ w)) + lambda_ * (np.linalg.norm(tx) ** 2) / 2
 
 def compute_gradient_sig(y,tx,w,lambda_):
